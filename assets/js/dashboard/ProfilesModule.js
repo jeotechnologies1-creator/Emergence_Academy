828c6837836183eadc427eb30cffa9acb187b85c
Your GitHub Usernamejeotechnologies1 <jeotechnologies1@gmail.com>
2026-08-22 22:00:14 +0100

    “update”

--- assets/js/dashboard/ProfilesModule.js
+++ assets/js/dashboard/ProfilesModule.js
@@ -37,12 +37,12 @@
                             </p>
                         </div>
 
-                        <button
-                            id="create-user-btn"
-                            class="bg-blue-600 text-white px-4 py-2 rounded-lg hover:bg-blue-700"
-                        >
-                            + Create User
-                        </button>
+                        // <button
+                        //     id="create-user-btn"
+                        //     class="bg-blue-600 text-white px-4 py-2 rounded-lg hover:bg-blue-700"
+                        // >
+                        //     + Create User
+                        // </button>
 
                     </div>
 
